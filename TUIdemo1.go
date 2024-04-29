9069f7c23369f6e4e1e2662e5de100b83e285f82
Nunya Business <nunya@business.org>
2024-04-29 17:58:31 -0400

    fairly complete TUI

--- TUIdemo1.go
+++ TUIdemo1.go
@@ -1,27 +1,31 @@
 package main
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/xuri/excelize/v2"
 	"log"
 )
 
 type DataScreen struct {
-	Company  *tview.InputField
-	JobTitle *tview.InputField
-	Location *tview.InputField
-	IsUSA    *tview.Checkbox
+	Company     *tview.InputField
+	JobTitle    *tview.InputField
+	Location    *tview.InputField
+	IsUSA       *tview.Checkbox
+	currentItem int
 }
 
 var dataDisplay DataScreen
 
 func main() {
 	dataToDisplay := getData("JobData.xlsx")
-	firstItem := dataToDisplay[1]
+
+	firstItem := dataToDisplay[dataDisplay.currentItem]
 	makeDataDisplay()
 	dataDisplay.Company.SetText(firstItem[0])
 	dataDisplay.JobTitle.SetText(firstItem[9])
 	dataDisplay.Location.SetText(firstItem[4])
+	dataDisplay.IsUSA.SetChecked(firstItem[3] == "us")
 	app := tview.NewApplication()
 	form := tview.NewForm()
 	form.SetBorder(true)
@@ -29,12 +33,49 @@ func main() {
 	form.AddFormItem(dataDisplay.JobTitle)
 	form.AddFormItem(dataDisplay.Location)
 	form.AddFormItem(dataDisplay.IsUSA)
+
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 'q' {
+			app.Stop()
+		}
+		return event
+	})
+
+	moveToPrev := func() {
+		dataDisplay.currentItem--
+		if dataDisplay.currentItem < 0 {
+			dataDisplay.currentItem = 0
+		}
+		currentItem := dataToDisplay[dataDisplay.currentItem]
+		dataDisplay.Company.SetText(currentItem[0])
+		dataDisplay.JobTitle.SetText(currentItem[9])
+		dataDisplay.Location.SetText(currentItem[4])
+		dataDisplay.IsUSA.SetChecked(currentItem[3] == "us")
+	}
+	moveToNext := func() {
+		dataDisplay.currentItem++
+		if dataDisplay.currentItem >= len(dataToDisplay) {
+			dataDisplay.currentItem = len(dataToDisplay) - 1
+		}
+		currentItem := dataToDisplay[dataDisplay.currentItem]
+		dataDisplay.Company.SetText(currentItem[0])
+		dataDisplay.JobTitle.SetText(currentItem[9])
+		dataDisplay.Location.SetText(currentItem[4])
+		dataDisplay.IsUSA.SetChecked(currentItem[3] == "us")
+	}
+	form.AddButton("Previous", moveToPrev)
+	form.AddButton("Next", moveToNext)
+	form.AddButton("Quit", app.Stop)
+	form.SetBorder(true)
+	form.SetButtonBackgroundColor(tcell.ColorDarkGoldenrod)
 	app.SetRoot(form, true)
 	err := app.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 }
+
+func updateUI() {}
 
 func makeDataDisplay() {
 	dataDisplay.Company = tview.NewInputField()
@@ -48,6 +89,7 @@ func makeDataDisplay() {
 	dataDisplay.Location.SetFieldWidth(40)
 	dataDisplay.IsUSA = tview.NewCheckbox()
 	dataDisplay.IsUSA.SetLabel("Is US Job:")
+
 }
 
 // the getData function from the cache Demo
